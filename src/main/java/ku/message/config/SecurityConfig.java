6ea882fd34fefed727ea73d278a06b1b03b16755
mmookptr <patteera.lik@ku.th>
2021-02-23 16:04:17 +0700

    google login

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -1,14 +1,12 @@
+
 package ku.message.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -19,25 +17,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/home", "/css/**", "/js/**").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
 
-        http.formLogin()
-                .defaultSuccessUrl("/message", true)
-                .and().logout();
-    }
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(
+                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user")
-                .password(encoder().encode("password"))
-                .roles("USER");
-    }
+                .and()
+                .oauth2Login()
+                .defaultSuccessUrl("/home").permitAll()
 
-    @Bean
-    public PasswordEncoder encoder() {
-        return new BCryptPasswordEncoder();
+                .and()
+                .logout()
+                .logoutSuccessUrl("/home").permitAll();
     }
 }
-
-
