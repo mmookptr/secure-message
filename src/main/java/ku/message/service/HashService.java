017e9ee5019e79bfd3591478946ea6618e34060f
mmookptr <patteera.lik@ku.th>
2021-02-23 16:00:48 +0700

    homework1

--- src/main/java/ku/message/service/HashService.java
+++ src/main/java/ku/message/service/HashService.java
@@ -15,7 +15,7 @@ public class HashService {
 
     public String getSalt() {
         SecureRandom random = new SecureRandom();
-        byte[] salt = new byte[16];
+        byte[] salt = new byte[32];
         random.nextBytes(salt);
         String encodedSalt = Base64.getEncoder().encodeToString(salt);
         return encodedSalt;
@@ -25,7 +25,7 @@ public class HashService {
         byte[] hashedValue = null;
 
         KeySpec spec = new PBEKeySpec(data.toCharArray(),
-                salt.getBytes(), 5000, 128);
+                salt.getBytes(), 100000, 128);
 
         try {
             SecretKeyFactory factory =
