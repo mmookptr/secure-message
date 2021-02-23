6d369d4d0f42a18439423323f5bb226ce2fc1eea
mmookptr <patteera.lik@ku.th>
2021-02-23 16:02:31 +0700

    labsheet3

--- src/main/java/ku/message/config/SecurityConfig.java
+++ src/main/java/ku/message/config/SecurityConfig.java
@@ -43,5 +43,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .ignoring()
                 .antMatchers("/h2-console/**");
     }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(12);
+    }
 }
 
--- src/main/java/ku/message/service/AuthenticationService.java
+++ src/main/java/ku/message/service/AuthenticationService.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -19,19 +20,21 @@ public class AuthenticationService  implements AuthenticationProvider {
     private UserRepository repository;
 
     @Autowired
-    private HashService hashService;
+    private PasswordEncoder passwordEncoder;
 
     @Override
-    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    public Authentication authenticate(Authentication authentication)
+            throws AuthenticationException {
+
         String username = authentication.getName();
         String password = authentication.getCredentials().toString();
 
         User user = repository.findByUsername(username);
         if (user != null) {
-            String salt = user.getSalt();
-            String hashedPassword = hashService.getHashedValue(password, salt);
-            if (user.getPassword().equals(hashedPassword)) {
-                return new UsernamePasswordAuthenticationToken(username, password, new ArrayList<>());
+            if (passwordEncoder.matches(password, user.getPassword())) {
+                return
+                        new UsernamePasswordAuthenticationToken(username,
+                                password, new ArrayList<>());
             }
         }
 
@@ -40,6 +43,7 @@ public class AuthenticationService  implements AuthenticationProvider {
 
     @Override
     public boolean supports(Class<?> authentication) {
-        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+        return
+                authentication.equals(UsernamePasswordAuthenticationToken.class);
     }
 }
